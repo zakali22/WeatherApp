8ed39ff6251fed0ae0ba00924926615aa2f54d83
Zakeria Ali <zak.fined@gmail.com>
2018-10-09 23:38:40 +0100

    Add boilerplate for the backend

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieSession = require("cookie-session");
+const session = require("express-session");
+const expressValidator = require("express-validator");
+
+const keys = require("./config/keys");
+
+// Header 'Allow-origin'
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
+// Mongoose connection
+const url = keys.mongo_uri;
+mongoose.connect(
+  url,
+  { useNewUrlParser: true }
+);
+
+// bodyParser + cookieParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express Messages
+app.use(require("connect-flash")());
+app.use((req, res, next) => {
+  res.locals.messages = require("express-messages")(req, res);
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      const namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+// Cookie Sessions
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days expiry
+    keys: [keys.cookieKey]
+  })
+);
+
+// Passport Initialization
+
+// File requires
