c63b7f68a62fbc4f1434d5fa86d64b2e5a2037b6
Zakeria Ali <zak.fined@gmail.com>
2018-10-11 01:34:20 +0100

    Add the News API

--- config/dev.js
+++ config/dev.js
@@ -6,5 +6,6 @@ module.exports = {
   facebookClientSecret: "c7a8a80d4315c80b61526b1322c4c230",
   mongo_uri: "mongodb://localhost:27017/weatherApp",
   cookieKey: "jdbjdbfhbvdfvdydfvbdyfvudvfu",
-  weatherID: "f8504abe6e01c1d06386e380802e8cba"
+  weatherID: "f8504abe6e01c1d06386e380802e8cba",
+  newsID: "df41219c89234f328a2bb11349c2c745"
 };
