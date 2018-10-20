f0b5335deb299eb07e1d3cc61aa64c9e20633081
Zakeria Ali <zak.fined@gmail.com>
2018-10-20 13:28:10 +0100

    Prepare for production

--- clientside/src/actions/weatherActions.js
+++ clientside/src/actions/weatherActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getWeather = city => async dispatch => {
   const res = await axios.post(
-    "https://afternoon-ridge-59283.herokuapp.com/getCurrentWeather",
+    "https://afternoon-ridge-59283.herokuapp.com/api/getCurrentWeather",
     {
       city_name: city
     }
